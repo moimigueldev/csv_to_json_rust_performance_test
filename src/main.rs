9edd22aac211518ca5c06e393b3315c4d7f67fc0
Moises Miguel <moimigueldev@gmail.com>
2024-11-09 23:51:06 -0600

    removed vec and added a buffer file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::fs::File;
+use std::{fs::File, io::BufWriter};
 
 use serde_json::{json, to_writer, Value};
+use std::io::Write;
 
 fn main() {
     let file_path = std::env::args()
@@ -8,7 +9,11 @@ fn main() {
         .unwrap_or("../data/chicago_crimes_2024_min.csv".to_string());
 
     let mut reader = csv::Reader::from_path(file_path).expect("Error while reading file");
-    let mut json_vec: Vec<Value> = vec![];
+    let output_file = File::create("./output_no_mem.json").expect("Unable to create");
+    let mut writer = BufWriter::new(&output_file);
+
+    write!(writer, "[").expect("Unable to write opening bracket");
+
     let headers: Vec<String> = reader
         .headers()
         .unwrap()
@@ -16,7 +21,7 @@ fn main() {
         .map(|h| h.to_string())
         .collect();
 
-    for line in reader.records() {
+    for (i, line) in reader.records().enumerate() {
         let readable_line = line
             .unwrap()
             .iter()
@@ -25,18 +30,18 @@ fn main() {
 
         let mut json_object = json!({});
 
-        for (index, h) in headers.iter().enumerate() {
+        for (j, h) in headers.iter().enumerate() {
             let map = json_object.as_object_mut().expect("Expected a jons object");
 
-            map.insert(
-                h.to_string(),
-                Value::String(readable_line[index].to_string()),
-            );
+            map.insert(h.to_string(), Value::String(readable_line[j].to_string()));
         }
 
-        json_vec.push(json_object);
+        if i > 0 {
+            write!(writer, ",").expect("Unable to write comma");
+        }
+
+        to_writer(&mut writer, &json_object).expect("Unable to write JSON data to file");
     }
 
-    let output_file = File::create("./output.json").expect("Unable to create");
-    to_writer(&output_file, &json_vec).expect("Unable to write JSON data to file");
+    write!(writer, "]").expect("Unable to write closing bracket");
 }
