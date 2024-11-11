97860dd5127c08c60f87b482b3c143bcdd8c2271
Moises Miguel <moimigueldev@gmail.com>
2024-11-11 14:39:42 -0600

    split rayon solution into batches

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{fs::File, io::BufWriter};
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+};
 
 use rayon::prelude::*;
 use serde_json::{json, to_writer, Value};
@@ -7,6 +10,12 @@ fn main() {
     let file_path = std::env::args()
         .nth(1)
         .unwrap_or("../data/chicago_crimes_2024_min.csv".to_string());
+
+    let batch_size = std::env::args()
+        .nth(2)
+        .unwrap_or("100".to_string())
+        .parse()
+        .unwrap_or(100);
 
     let output_file = File::create("./output.json").expect("Unable to create");
     let mut reader = csv::Reader::from_path(file_path).expect("Error while reading file");
@@ -22,21 +31,41 @@ fn main() {
         .collect::<Result<_, _>>()
         .expect("Failed to read records");
 
-    let json_list: Vec<_> = records
-        .into_par_iter()
-        .map(|line| {
-            let mut json_object = json!({});
-            for (index, header) in headers.iter().enumerate() {
-                let map = json_object.as_object_mut().expect("Expected a jons object");
-                map.insert(
-                    header.to_string(),
-                    Value::String(line.get(index).unwrap().to_string()),
-                );
+    let batches: Vec<_> = records
+        .par_chunks(batch_size)
+        .map(|chunk| {
+            let mut batch = Vec::new();
+
+            for line in chunk {
+                let mut json_object = json!({});
+                for (index, header) in headers.iter().enumerate() {
+                    let map = json_object.as_object_mut().expect("Expected a jons object");
+                    map.insert(
+                        header.to_string(),
+                        Value::String(line.get(index).unwrap().to_string()),
+                    );
+                }
+
+                batch.push(json_object);
             }
 
-            json_object
+            batch
         })
         .collect();
 
-    to_writer(&mut writer, &json_list).expect("Unable to write final batch to file");
+    let mut first_batch = true;
+
+    write!(writer, "[").expect("Unable to write closing bracket");
+    for batch in batches {
+        for json_obect in batch {
+            if !first_batch {}
+            write!(&mut writer, ",").expect("Unable to write comma");
+            first_batch = false;
+            to_writer(&mut writer, &json_obect).expect("Unable to write final batch to file");
+        }
+    }
+
+    write!(writer, "]").expect("Unable to write closing bracket");
+
+    // to_writer(&mut writer, &json_list).expect("Unable to write final batch to file");
 }
