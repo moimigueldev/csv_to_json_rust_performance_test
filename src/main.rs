10d4f753f8463e1f7ed7f18a3d1cb8ccebd635dc
Moises Miguel <moimigueldev@gmail.com>
2024-11-10 19:40:07 -0600

    impl initial solution

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{fs::File, io::BufWriter};
+use std::{
+    fs::File,
+    io::BufWriter,
+    sync::{Arc, Mutex},
+    thread,
+};
 
 use serde_json::{json, to_writer, Value};
 use std::io::Write;
@@ -9,39 +14,58 @@ fn main() {
         .unwrap_or("../data/chicago_crimes_2024_min.csv".to_string());
 
     let mut reader = csv::Reader::from_path(file_path).expect("Error while reading file");
-    let output_file = File::create("./output_no_mem.json").expect("Unable to create");
-    let mut writer = BufWriter::new(&output_file);
-
-    write!(writer, "[").expect("Unable to write opening bracket");
-
     let headers: Vec<String> = reader
         .headers()
         .unwrap()
         .iter()
         .map(|h| h.to_string())
         .collect();
+    let headers = Arc::new(headers);
+    let output_file = File::create("./output_no_mem.json").expect("Unable to create");
+    let mut writer = BufWriter::new(&output_file);
+    let mut json_list: Arc<Mutex<Vec<Value>>> = Arc::new(Mutex::new(Vec::new()));
+    let mut thread_list = vec![];
+    let num_of_threads = 2;
+    let mut records: Vec<_> = reader
+        .records()
+        .collect::<Result<_, _>>()
+        .expect("failed to read records");
 
-    for (i, line) in reader.records().enumerate() {
-        let readable_line = line
-            .unwrap()
-            .iter()
-            .map(|l| l.to_string())
-            .collect::<Vec<_>>();
+    let chunk_size = records.len() / num_of_threads;
 
-        let mut json_object = json!({});
+    for i in 0..num_of_threads {
+        let json_list_copy = json_list.clone();
+        let headers_copy = headers.clone();
 
-        for (j, h) in headers.iter().enumerate() {
-            let map = json_object.as_object_mut().expect("Expected a jons object");
+        let chunk = if records.len() > chunk_size {
+            records.split_off(records.len() - chunk_size)
+        } else {
+            // Take the remainignt elements if less than chunk size
+            std::mem::take(&mut records)
+        };
 
-            map.insert(h.to_string(), Value::String(readable_line[j].to_string()));
-        }
+        let handle = thread::spawn(move || {
+            let mut lock_list = json_list_copy.lock().unwrap();
 
-        if i > 0 {
-            write!(writer, ",").expect("Unable to write comma");
-        }
+            for item in chunk.into_iter() {
+                let mut json_object = json!({});
+                for (j, h) in headers_copy.iter().enumerate() {
+                    let map = json_object.as_object_mut().expect("Expected a jons object");
+                    map.insert(
+                        h.to_string(),
+                        Value::String(item.get(j).unwrap().to_string()),
+                    );
+                }
+                lock_list.push(json_object);
+            }
+        });
 
-        to_writer(&mut writer, &json_object).expect("Unable to write JSON data to file");
+        thread_list.push(handle);
     }
 
-    write!(writer, "]").expect("Unable to write closing bracket");
+    for thread in thread_list.into_iter() {
+        thread.join().unwrap();
+    }
+
+    to_writer(&mut writer, &*json_list.lock().unwrap()).expect("Unable to write JSON data to file");
 }
