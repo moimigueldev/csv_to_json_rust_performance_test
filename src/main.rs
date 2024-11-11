2df6f3ae72f664f0d5f79e3fb73d7a7346bb5266
Moises Miguel <moimigueldev@gmail.com>
2024-11-11 12:57:42 -0600

    created channels solution

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() {
 
     let output_file = File::create("./output.json").expect("Unable to create");
     let mut writer = BufWriter::new(&output_file);
-    let mut json_list: Vec<Value> = Vec::new();
+    // let json_list: Vec<Value> = Vec::new();
 
     let mut records: Vec<_> = reader
         .records()
@@ -46,7 +46,7 @@ fn main() {
 
     let (tx, rx) = mpsc::channel();
 
-    for thread_index in 0..num_of_threads {
+    for _ in 0..num_of_threads {
         // println!("Looping:: current thread index: {thread_index}");
         let headers_copy = headers.clone();
         let tx_copy = tx.clone();
@@ -82,79 +82,17 @@ fn main() {
 
     drop(tx);
 
+    let mut batch = Vec::with_capacity(100); // Adjust batch size as needed
     for received in rx {
-        json_list.push(received);
+        batch.push(received);
+        if batch.len() >= 100 {
+            // When batch is full
+            to_writer(&mut writer, &batch).expect("Unable to write batch to file");
+            batch.clear(); // Clear for the next batch
+        }
     }
 
-    to_writer(&mut writer, &json_list).expect("Unable to write JSON data to file");
+    if !batch.is_empty() {
+        to_writer(&mut writer, &batch).expect("Unable to write final batch to file");
+    }
 }
-
-// fn main() {
-//     let file_path = std::env::args()
-//         .nth(1)
-//         .unwrap_or("../data/chicago_crimes_2024_min.csv".to_string());
-//
-//     let mut reader = csv::Reader::from_path(file_path).expect("Error while reading file");
-//     let headers: Vec<String> = reader
-//         .headers()
-//         .unwrap()
-//         .iter()
-//         .map(|h| h.to_string())
-//         .collect();
-//     let headers = Arc::new(headers);
-//     let output_file = File::create("./output_no_mem.json").expect("Unable to create");
-//     let mut writer = BufWriter::new(&output_file);
-//     let mut json_list: Arc<Mutex<Vec<Value>>> = Arc::new(Mutex::new(Vec::new()));
-//     let mut thread_list = vec![];
-//     let num_of_threads = 4;
-//     let mut records: Vec<_> = reader
-//         .records()
-//         .collect::<Result<_, _>>()
-//         .expect("failed to read records");
-//
-//     let chunk_size = records.len() / num_of_threads;
-//     dbg!(records.len());
-//     dbg!(&chunk_size);
-//
-//     for i in 0..num_of_threads {
-//         let json_list_copy = json_list.clone();
-//         let headers_copy = headers.clone();
-//
-//         let chunk = if records.len() > chunk_size {
-//             records.split_off(records.len() - chunk_size)
-//         } else {
-//             // Take the remainignt elements if less than chunk size
-//             std::mem::take(&mut records)
-//         };
-//
-//         let handle = thread::spawn(move || {
-//             println!("Started thread: {}", i); // Immediate println! at the start of each thread
-//
-//             // Simulate processing time to observe parallel execution
-//             std::thread::sleep(std::time::Duration::from_secs(1));
-//             let mut lock_list = json_list_copy.lock().unwrap();
-//
-//             for item in chunk.into_iter() {
-//                 let mut json_object = json!({});
-//                 for (j, h) in headers_copy.iter().enumerate() {
-//                     let map = json_object.as_object_mut().expect("Expected a jons object");
-//                     map.insert(
-//                         h.to_string(),
-//                         Value::String(item.get(j).unwrap().to_string()),
-//                     );
-//                 }
-//                 lock_list.push(json_object);
-//             }
-//
-//             println!("Finished processing in thread: {}", i);
-//         });
-//
-//         thread_list.push(handle);
-//     }
-//
-//     for thread in thread_list.into_iter() {
-//         thread.join().unwrap();
-//     }
-//
-//     to_writer(&mut writer, &*json_list.lock().unwrap()).expect("Unable to write JSON data to file");
-// }
